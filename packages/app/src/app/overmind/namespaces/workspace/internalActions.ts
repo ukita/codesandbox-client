2da2c8689e2f8881d7252bfa3837ce30eb12d94f
christianalfoni <christianalfoni@gmail.com>
2019-07-08 16:39:35 +0200

    so much refactoring...

--- packages/app/src/app/overmind/namespaces/workspace/internalActions.ts
+++ packages/app/src/app/overmind/namespaces/workspace/internalActions.ts
@@ -2,7 +2,7 @@ import { Action } from 'app/overmind';
 import getItems from './items';
 import { Sandbox } from '@codesandbox/common/lib/types';
 
-export const setWorkspace: Action<Sandbox> = ({ state }, sandbox) => {
+export const configureWorkspace: Action<Sandbox> = ({ state }, sandbox) => {
   state.workspace.project.title = sandbox.title || '';
   state.workspace.project.description = sandbox.description || '';
   state.workspace.project.alias = sandbox.alias || '';
